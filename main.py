6b7c98e0aaadfb5bf844104340b526478bb733b6
bouleit-repo <59412427+bouleit-repo@users.noreply.github.com>
2020-01-01 17:09:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-some_error_test
+some_error_test 2
 thefile = open('/home/fb/bouleit/bouleit.github.io/README.md', 'w', 1)
 thefile.write('# Some big title' + '\n')
 thefile.write('It works NOW ##############' + '\n')
