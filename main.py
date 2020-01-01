66c15aec1d6f7d47d021175d6ce8ac2389330429
bouleit-repo <59412427+bouleit-repo@users.noreply.github.com>
2020-01-01 23:05:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import csv
 
 thefile = open('/home/fb/bouleit/bouleit.github.io/README.md', 'w', 1)
 
-thefile.write('# Boule iT Turnier' + '\n')
+thefile.write('# Boule iT Turnier v0.1' + '\n')
 thefile.write('## Schweizer System mit Buchholz-Wertung' + '\n')
-thefile.write('## Teilnehmerliste:' + '\n\n')
+thefile.write('## Teilnehmerliste:' + '\n')
 
+# render table from .csv
 with open('teilnehmer.csv') as csvfile:
     reader = csv.DictReader(csvfile)
-    thefile.write('| ' + 'NR' + ' | ' + 'NAME' + ' | ' + 'VEREIN' + ' | ' + 'SPIELER' + ' |' +'\n')
+    thefile.write('\n')
+    thefile.write('|' + 'NR' + '|' + 'NAME' + '|' + 'VEREIN' + '|' + 'SPIELER' + '|' +'\n')
     for row in reader:
-        thefile.write('| ' + row['NR'] + ' | ' + row['NAME'] + ' | ' + row['VEREIN'] + ' | ' + row['SPIELER'] + ' |' +'\n')
+        thefile.write('|' + row['NR'] + '|' + row['NAME'] + '|' + row['VEREIN'] + '|' + row['SPIELER'] + '|' +'\n')
+    thefile.write('\n')
 
-thefile.write('\n')
+# are there any spare players?
+thefile.write('## Freilos:' + '\n')
+
 thefile.close()
