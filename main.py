4d2886e3be5a732a155ed01fc10af353b171ce32
bouleit-repo <59412427+bouleit-repo@users.noreply.github.com>
2020-01-01 20:14:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ thefile.write('# Boule iT Turnier' + '\n')
 thefile.write('## Schweizer System mit Buchholz-Wertung' + '\n')
 thefile.write('## Teilnehmerliste:' + '\n')
 
-with open('/home/fb/bouleit/bouleit.github.io/teilnehmer.csv') as csvfile:
+with open('teilnehmer.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     thefile.write('|' + 'NR' + '|' + 'NAME' + '|' + 'VEREIN' + '|' + 'SPIELER' + '|' +'\n')
     for row in reader:
